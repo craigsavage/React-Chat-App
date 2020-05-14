4b275a98106530353b2f28f3898b285b238036e9
craigsavage <craig.savage@ontariotechu.net>
2020-05-14 00:36:20 -0400

    Initiallized Client and Server + Added basic connection setup for server side

---
+++ client/src/components/Join.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join = () => {
+    return (
+        <div>
+            Hey
+        </div>
+    )
+}
+
+export default Join;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express'),
+      app     = express();
+
+const http = require('http').createServer(app),
+      io   = require('socket.io')(http),
+      cors = require('express');
+
+app.use(cors());
+app.use(express.json()); //Used to parse JSON bodies
+
+// Routes
+const router = require('./router/router');
+
+app.use(router);
+
+// Socket
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+// SERVER
+const PORT = process.env.PORT || 5000;
+
+http.listen(PORT, () => {
+    console.log(`Server successfully started on port: ${PORT}`);
+})
